56537fa33153967ea21802d260567a219ff7f5d2
Radostin Gerdzhikov <rgerdzhikov@gmail.com>
2017-06-05 13:57:41 +0300

    Add files via upload

---
+++ 13-AreaofFigures.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_AreaofFigures
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine().ToLower();
+          
+            if (figure == "square")
+            {
+                double size1 = double.Parse(Console.ReadLine());
+                double squareArea = (size1 * size1);
+                Console.WriteLine(squareArea);
+
+            }
+            else if (figure == "rectangle")
+            {
+                double size2 = double.Parse(Console.ReadLine());
+                double size3 = double.Parse(Console.ReadLine());
+                double rectangleArea = size2 * size3;
+                Console.WriteLine(rectangleArea);
+            }
+
+            else if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                double circleArea = Math.PI * r * r;
+                Console.WriteLine("{0:F3}", circleArea);
+
+            }
+
+            else if (figure == "triangle")
+            {
+                double size5 = double.Parse(Console.ReadLine());
+                double size6 = double.Parse(Console.ReadLine());
+                double triAngelArea = (size5 * size6)/2;
+                Console.WriteLine(triAngelArea);
+            }
+            
+                
+
+
+
+
+        }
+    }
+}
