ff19630a39fd47c0e465024b2d74a7472f0d6703
Radostin Gerdzhikov <rgerdzhikov@gmail.com>
2017-06-05 13:47:14 +0300

    Add files via upload

---
+++ OddandEven.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_OddandEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            //  %  показателя проверява само остатъка ==  )
+            {
+                Console.WriteLine("even");
+            }
+            else 
+            {
+                Console.WriteLine("odd");
+            }
+            
+        }
+    }
+}
