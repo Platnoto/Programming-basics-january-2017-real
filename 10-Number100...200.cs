73a80ad58183db336fb59578b0c523e9c72eb902
Radostin Gerdzhikov <rgerdzhikov@gmail.com>
2017-06-05 13:55:38 +0300

    Add files via upload

---
+++ 10-Number100...200.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Number100._._._200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number <= 99)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if (number >99 && number <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else 
+            {
+                Console.WriteLine("Greater than 200");
+            }
+
+
+
+        }
+    }
+}
