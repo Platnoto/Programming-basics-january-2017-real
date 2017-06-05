89633b4d46c373aed8c0a6ca6945a9b59e4dcb67
Radostin Gerdzhikov <rgerdzhikov@gmail.com>
2017-06-05 13:48:01 +0300

    Add files via upload

---
+++ GreaterNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_GreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number1 >= number2)
+            {
+                Console.WriteLine("Greter number is " + number1);
+            }
+            else
+            {
+                Console.WriteLine("Greter number is {0}", number2);
+            }
+      
+
+        }
+    }
+}
