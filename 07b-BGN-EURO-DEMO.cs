edceedb2636f3ba03465cccd8498cef94b1df4ab
Radostin Gerdzhikov <rgerdzhikov@gmail.com>
2017-06-05 13:51:32 +0300

    Add files via upload

---
+++ 07b-BGN-EURO-DEMO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07b_BGN_EURO_DEMO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
