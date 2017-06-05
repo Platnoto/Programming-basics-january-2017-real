f3aef6fa7f2145ae5c0b55549c5f6ba82203e4e5
Radostin Gerdzhikov <rgerdzhikov@gmail.com>
2017-06-05 13:58:26 +0300

    Add files via upload

---
+++ 14-TimeaAnd15Minutes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_TimeaAnd15Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var hours = int.Parse(Console.ReadLine());
+            //  var minutes = int.Parse(Console.ReadLine());
+            // DateTime time = new DateTime();
+            // time = time.AddHours(hours);
+            // time = time.AddMinutes(minutes + 15);
+            //  Console.WriteLine(time.ToString("H:mm")):
+
+            var hours = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+            DateTime time = new DateTime();
+            time = time.AddHours(hours);
+            time = time.AddMinutes(minutes + 15);
+            Console.WriteLine(time.ToString("H:mm"));
+
+
+
+        }
+    }
+}
