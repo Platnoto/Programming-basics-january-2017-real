f5c8bc89a25b5642e2504b7194d7335d19b217a8
Radostin Gerdzhikov <rgerdzhikov@gmail.com>
2017-06-05 13:52:24 +0300

    Add files via upload

---
+++ 07-SumSeconds.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sec1 = int.Parse(Console.ReadLine());
+            int sec2 = int.Parse(Console.ReadLine());
+            int sec3 = int.Parse(Console.ReadLine());
+
+            int sumSec = sec1 + sec2 + sec3;
+
+
+        double min = sumSec / 60;
+      double  sec = sumSec % 60;
+
+            if (sec < 10)
+            {
+                Console.WriteLine("{0}:0{1}", min, sec);
+            }
+            else
+            {
+                Console.WriteLine("{0}:{1}", min, sec);
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+            
+
+
+
+
+        
