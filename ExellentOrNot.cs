66c18692020e81efc2cdc87599cca5b8b4ae59fb
Radostin Gerdzhikov <rgerdzhikov@gmail.com>
2017-06-05 13:46:26 +0300

    Add files via upload

---
+++ ExellentOrNot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ExcellentorNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double schoolGrate = double.Parse(Console.ReadLine());
+            if ( schoolGrate >= 5.50 )
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else
+            {
+                Console.WriteLine("Not excellent.");
+            }
+        }
+    }
+}
